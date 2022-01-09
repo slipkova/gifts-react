f42af5177118dea2df069a00df15baa82f0170f3
elis <eliska.slipkova@gmail.com>
2022-01-10 00:56:35 +0100

    Popup form

--- src/components/forms/AddListPopupForm.jsx
+++ src/components/forms/AddListPopupForm.jsx
@@ -1,11 +1,12 @@
 import React, {Component, Fragment} from "react";
 import "./../../style/popup.css"
+import Input from "./formInput";
 
 export default class ListPopupForm extends Component{
     render() {
         return (this.props.trigger) ? (<div className={"popup"}>
             <div className={"popup-inner"}>
-                <div>Popup!</div>
+                <Input name={"list-name"} label={"List name"} onChange={""} value={""}/>
                 <button className={"popup-btn"} onClick={this.props.onPress}>Ok</button>
             </div>
         </div>) : "";
